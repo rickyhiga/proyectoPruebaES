96404a61615de00675db4fc9aba04bc3339ccb59
Your Name <rickyssht@gmail.com>
2014-09-27 19:46:57 -0300

    subiendo

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+import frames.FrmEditorTexto;
+import java.io.IOException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author juan
+ */
+public class Main{
+
+
+
+    public static void main(String[] ar) throws IOException {
+
+          FrmEditorTexto frm=new FrmEditorTexto();
+          frm.show();
+          
+    }
+
+    
+}
---
+++ src/main/java/controllers/EditorTexto.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+import javax.swing.JTextArea;
+
+import org.languagetool.JLanguageTool;
+import org.languagetool.language.Spanish;
+import org.languagetool.rules.RuleMatch;
+import java.awt.Color;
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.Highlighter;
+import javax.swing.text.Highlighter.HighlightPainter;
+
+/**
+ *
+ * @author user
+ */
+public class EditorTexto {
+
+    private JLanguageTool langT;
+    private String acu = "";
+
+    public void corregir(JTextArea sin, JTextArea cor) throws IOException, BadLocationException {
+        cor.setText("");
+        int p0, p1;
+        //sin.setHighlighter(null);
+        Highlighter highlighter = sin.getHighlighter();
+        
+        HighlightPainter painter = new DefaultHighlighter.DefaultHighlightPainter(Color.pink);
+        HighlightPainter painter2 = new DefaultHighlighter.DefaultHighlightPainter(Color.YELLOW);
+        //HighlightPainter painter3 = new DefaultHighlighter.DefaultHighlightPainter(Color.WHITE);
+        //highlighter.addHighlight(0, sin.getText().length(), painter3);
+        highlighter.removeAllHighlights();
+        langT = new JLanguageTool(new Spanish());
+        langT.activateDefaultPatternRules();
+        List<RuleMatch> matches;
+        try {
+            matches = langT.check(sin.getText());
+            for (RuleMatch match : matches) {
+
+                acu += " Error en la línea "
+                        + match.getLine() + ", columna "
+                        + match.getColumn() + ": ";
+                acu += " Contexto(";
+                int aux = match.getToPos() - match.getFromPos();
+                acu += sin.getText(match.getFromPos(), aux)+")";
+
+                acu += match.getMessage()
+                        + (" Corrección Sugerida: " + match.getSuggestedReplacements() + "\n ");
+
+                p0 = match.getFromPos();
+                p1 = match.getToPos();
+                if (match.getShortMessage().equalsIgnoreCase("Error de ortografía")) {
+                    highlighter.addHighlight(p0, p1, painter);
+                    sin.append(" ");
+                    //highlighter.changeHighlight(painter, p0, p1);
+                    
+                } else {
+                    highlighter.addHighlight(p0, p1, painter2);
+                    sin.append(" ");
+                }
+                
+
+
+            }
+            cor.setText(acu);
+            
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+}
